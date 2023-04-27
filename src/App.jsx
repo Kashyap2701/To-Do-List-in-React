422de90179dc74751f77990a397aec92ee92d7c4
Kashyap Patel <kashyap.p@simformsolutions.com>
2023-04-27 11:21:02 +0530

    fix expiration todo list

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
     if (lastClearedDate !== currentDate) {
       localStorage.clear();
       localStorage.setItem('lastClearedDate', currentDate);
+      setTodos([]);
     }
   },[])
 
